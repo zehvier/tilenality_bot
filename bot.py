10c0c5d097d339124d016e0d40c273307bc209d0
Xavier <zehvier@Xaviers-MacBook-Pro.local>
2020-09-05 23:42:40 +0800

    commit

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://intense-savannah-31598.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://tile-tonality-nus.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
